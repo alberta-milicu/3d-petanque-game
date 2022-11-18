300cd3f2265c9fe0f4cb83fee725dfbcb2f6c34a
Alberta Milicu <alberta.milicu@gmail.com>
2022-11-18 11:40:37 +0200

    Initial commit

---
+++ Ball/ball.cpp
@@ -0,0 +1,125 @@
+# include "ball.h"
+
+glm::vec3 xRotate(1.0f, 0.0f, 0.0f);
+
+Ball::Ball()
+{
+	this->ballPosition = glm::vec3(0.0f, 0.0f, 0.0f);
+}
+
+Ball::Ball(glm::vec3 ballPosition, int throwTrue, int calibrateTrue)
+{
+	this->ballPosition = ballPosition;
+	this->throwTrue = throwTrue;
+	this->calibrateTrue = calibrateTrue;
+}
+
+Ball::~Ball()
+{
+}
+
+void Ball::drawBall(glm::mat4 MVP, glm::mat4 projection, glm::mat4 view, glm::mat4 model, GLuint programID)
+{
+
+	unsigned int transformLoc0 = glGetUniformLocation(programID, "model");
+	glUniformMatrix4fv(transformLoc0, 1, GL_FALSE, glm::value_ptr(model));
+
+	unsigned int transformLoc1 = glGetUniformLocation(programID, "transform");
+	glUniformMatrix4fv(transformLoc1, 1, GL_FALSE, glm::value_ptr(MVP));
+
+	glDrawArrays(GL_QUADS, 0, 24);
+}
+
+glm::mat4 Ball::ballRotate(glm::mat4 model, float throwDist)
+{
+	if (this->getBallPositionZ() > throwDist)
+		model = glm::rotate(model, (float)glfwGetTime() * -150, glm::vec3(1.0f, 0.0f, 0.0f));
+	return model;
+}
+
+glm::mat4 Ball::ballAdvance(glm::mat4 model, float throwDist)
+{
+	if (this->getBallPositionZ() > throwDist)
+		model = glm::translate(model, this->getBallPosition());
+	else model = glm::translate(model, glm::vec3(this->getBallPositionX(), this->getBallPositionY(), throwDist));
+	return model;
+}
+
+glm::mat4 Ball::ballThrow(glm::mat4 model, float throwDist)
+{
+	//this->setBallPositionZ(this->getBallPositionZ() - 0.1f);
+	//this->ballPosition.z = this->ballPosition.z - 0.1f;
+	model = this->ballAdvance(model, throwDist);
+	model = this->ballRotate(model, throwDist);
+	return model;
+}
+
+glm::mat4 Ball::ballCalibrate(glm::mat4 model)
+{
+	model = glm::translate(model, this->getBallPosition());
+	return model;
+}
+
+
+
+glm::vec3 Ball::getBallPosition()
+{
+	return this->ballPosition;
+}
+
+float Ball::getBallPositionX()
+{
+	return this->ballPosition.x;
+}
+
+float Ball::getBallPositionY()
+{
+	return this->ballPosition.y;
+}
+
+float Ball::getBallPositionZ()
+{
+	return this->ballPosition.z;
+}
+
+int Ball::getBallThrowTrue()
+{
+	return this->throwTrue;
+}
+
+int Ball::getBallCalibrateTrue()
+{
+	return this->calibrateTrue;
+}
+
+
+
+void Ball::setBallPosition(glm::vec3 ballPosition)
+{
+	this->ballPosition = ballPosition;
+}
+
+void Ball::setBallPositionX(float x)
+{
+	this->ballPosition.x = x;
+}
+
+void Ball::setBallPositionY(float y)
+{
+	this->ballPosition.y = y;
+}
+
+void Ball::setBallPositionZ(float z)
+{
+	this->ballPosition.z = z;
+}
+
+void Ball::setBallThrowTrue(int throwTrue)
+{
+	this->throwTrue = throwTrue;
+}
+
+void Ball::setBallCalibrateTrue(int calibrateTrue)
+{
+	this->calibrateTrue = calibrateTrue;
+}
---
+++ main.cpp
@@ -0,0 +1,391 @@
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+// Include Camera
+#include "Camera\camera.h"
+
+// Include GLEW
+#include "dependente\glew\glew.h"
+
+// Include GLFW
+#include "dependente\glfw\glfw3.h"
+
+// Include GLM
+#include "dependente\glm\glm.hpp"
+#include "dependente\glm\gtc\matrix_transform.hpp"
+#include "dependente\glm\gtc\type_ptr.hpp"
+
+#include "shader.hpp"
+
+#include "Ball/ball.h"
+
+//variables
+GLFWwindow* window;
+const int width = 1024, height = 768;
+
+glm::vec3 cameraPos = glm::vec3(0.0f, 1.3f, 350.0f);
+glm::vec3 cameraDir = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+Camera camera(cameraPos, cameraDir, cameraUp);
+
+// time between current frame and last frame
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+// lighting
+
+glm::vec3 lightPos(-3.0f, 0.0f, 2.0f);
+glm::vec3 lightColor(1.0f, 1.0f, 0.0f);
+glm::vec3 fragPos(1.0f, 1.0f, 0.0f);
+
+glm::vec3 viewPos(camera.getCameraPosition());
+
+
+void processKeyboardInput();
+
+void setFrames()
+{
+	float currentFrame = glfwGetTime();
+	deltaTime = currentFrame - lastFrame;
+	lastFrame = currentFrame;
+
+}
+
+void setLight(GLuint programID)
+{
+	unsigned int transformLoc2 = glGetUniformLocation(programID, "lightColor");
+	glUniform3fv(transformLoc2, 1, glm::value_ptr(lightColor));
+
+
+	unsigned int transformLoc3 = glGetUniformLocation(programID, "lightPos");
+	glUniform3fv(transformLoc3, 1, glm::value_ptr(lightPos));
+
+
+	unsigned int transformLoc4 = glGetUniformLocation(programID, "viewPos");
+	glUniform3fv(transformLoc4, 1, glm::value_ptr(viewPos));
+}
+
+void setWindow(GLFWwindow* window)
+{
+	// Swap buffers
+	glfwSwapBuffers(window);
+
+	// Check for events
+	glfwPollEvents();
+	processKeyboardInput();
+
+	//enable depth buffer
+	glEnable(GL_DEPTH_TEST);
+
+	// Clear the screen
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void useShaderPack(GLuint programID, GLuint vao)
+{
+	glUseProgram(programID);
+
+	glBindVertexArray(vao);
+}
+
+void animateBall(Ball allyBall1, glm::mat4 projection, glm::mat4 view, glm::mat4 model, glm::mat4 MVP, GLuint programID)
+{		
+
+		model = glm::mat4(1.0);
+		
+		model = allyBall1.ballThrow(model, -600.f);
+
+		MVP = projection * view * model;
+
+		allyBall1.drawBall(MVP, projection, view, model, programID);
+
+}
+
+
+void window_callback(GLFWwindow* window, int new_width, int new_height)
+{
+	glViewport(0, 0, new_width, new_height);
+}
+
+
+int main(void)
+{
+	// Initialise GLFW
+	if (!glfwInit())
+	{
+		fprintf(stderr, "Failed to initialize GLFW\n");
+		return -1;
+	}
+
+	// Open a window and create its OpenGL context
+	window = glfwCreateWindow(width, height, "Lighting", NULL, NULL);
+	if (window == NULL) {
+		fprintf(stderr, "Failed to open GLFW window.");
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	// Initialize GLEW
+	glewExperimental = true; // Needed for core profile
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		glfwTerminate();
+		return -1;
+	}
+
+	//specify the size of the rendering window
+	glViewport(0, 0, width, height);
+
+	// Dark blue background
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	// Load shader
+	GLuint programID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
+	GLuint programIDLight = LoadShaders("LightVertexShader.vertexshader", "LightFragmentShader.fragmentshader");
+
+	GLfloat vertices[] = {
+		//we add coordinates for NORMALS 
+		//back
+		// position			//normals		
+		-0.5f, -0.5f, -0.5f, 0.0f,  0.0f, -1.0f,  //0
+		0.5f, -0.5f, -0.5f, 0.0f,  0.0f, -1.0f,  //1
+		0.5f,  0.5f, -0.5f, 0.0f,  0.0f, -1.0f,  //2
+		-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  //3
+
+		//front
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  //4
+		0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  //5
+		0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  //6
+		-0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f, //7
+
+		 //left
+		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f, //8
+		-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f, //9
+		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f, //10
+		-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f, //11
+
+		// right
+		0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f, //12
+		0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f, //13
+		0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f, //14
+		0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f, //15
+
+		 //bottom
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f, //16
+		0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f, //17
+		0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f, //18
+		-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,//19
+
+		 //top
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f, //20
+		0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f, //21
+		0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f, //22
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,  0.0f,  //23
+	};
+	
+
+	//glm::vec3 cubePositions[] = {
+	//	//glm::vec3(0.0f,  0.0f,  0.0f),
+	//	//glm::vec3(2.0f,  5.0f, -15.0f),
+	//	//glm::vec3(-1.5f, -2.2f, -2.5f),
+	//	/*glm::vec3(-3.8f, -2.0f, -12.3f),
+	//	glm::vec3(2.4f, -0.4f, -3.5f),
+	//	glm::vec3(-1.7f,  3.0f, -7.5f),
+	//	glm::vec3(1.3f, -2.0f, -2.5f),
+	//	glm::vec3(1.5f,  2.0f, -2.5f),
+	//	glm::vec3(1.5f,  0.2f, -1.5f),
+	//	glm::vec3(-1.3f,  1.0f, -1.5f)*/
+	//};
+
+	// A Vertex Array Object (VAO) is an object which contains one or more Vertex Buffer Objects and is designed to store the information for a complete rendered object. 
+	GLuint vbo, vao;
+	glGenVertexArrays(1, &vao);
+	glGenBuffers(1, &vbo);
+
+	glBindVertexArray(vao);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// 1rst attribute buffer : vertices position
+	glVertexAttribPointer(
+		0,                  // attribute 0, must match the layout in the shader.
+		3,                  // size of each attribute
+		GL_FLOAT,           // type
+		GL_FALSE,           // normalized?
+		6 * sizeof(float),     // stride
+		0            // array buffer offset
+	);
+	glEnableVertexAttribArray(0);
+
+	// 2nd attribute buffer : normal coords
+	glVertexAttribPointer(
+		1,                  // attribute 1, must match the layout in the shader.
+		3,                  // size of each attribute
+		GL_FLOAT,           // type
+		GL_FALSE,           // normalized?
+		6 * sizeof(float),                  // stride
+		(void*)(3 * sizeof(float))            // array buffer offset
+	);
+	glEnableVertexAttribArray(1);
+	
+	glfwSetFramebufferSizeCallback(window, window_callback);
+
+
+
+	Ball allyBall1 = Ball(glm::vec3(0.0f, 0.0f, 0.0f),0,0);
+
+
+	// Check if the window was closed
+	while (!glfwWindowShouldClose(window) && glfwGetKey(window,GLFW_KEY_ESCAPE)!=GLFW_PRESS)
+	{	
+
+		setWindow(window);
+
+		setFrames();
+
+		glm::mat4 model;
+	
+		//Adding camera
+		glm::mat4 view = glm::lookAt(camera.getCameraPosition(),
+			camera.getCameraPosition() + camera.getCameraViewDirection(), camera.getCameraUp());
+
+		glm::mat4 projection;
+		projection = glm::perspective(glm::radians(45.0f), (float)width / height, 0.1f, 10000.0f);
+
+		//glm::mat4 MVP;
+
+
+		glm::mat4 MVP;
+
+		useShaderPack(programID, vao);
+
+		if (!allyBall1.getBallThrowTrue() && glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS)
+		{
+			allyBall1.setBallThrowTrue(1);
+			allyBall1.setBallCalibrateTrue(1);
+		}
+
+		if (!allyBall1.getBallThrowTrue() && !allyBall1.getBallCalibrateTrue() 
+			&& glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+		{
+			allyBall1.setBallPositionX(allyBall1.getBallPositionX() - 0.007f);
+			allyBall1.ballCalibrate(model);
+
+		}
+
+		if (!allyBall1.getBallThrowTrue() && !allyBall1.getBallCalibrateTrue() 
+			&& glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+		{
+			allyBall1.setBallPositionX(allyBall1.getBallPositionX() + 0.007f);
+			allyBall1.ballCalibrate(model);
+		}
+
+		if (!allyBall1.getBallThrowTrue() && !allyBall1.getBallCalibrateTrue()) 
+		{
+
+			model = allyBall1.ballCalibrate(model);
+
+	
+		}
+
+		if (allyBall1.getBallThrowTrue() && allyBall1.getBallCalibrateTrue())
+		{
+			allyBall1.setBallPositionZ(allyBall1.getBallPositionZ() - 0.07f);
+			animateBall(allyBall1, projection, view, model, MVP, programID);
+		}
+		else 
+		{
+			
+			model = glm::translate(model, allyBall1.getBallPosition());
+
+			MVP = projection * view * model;
+
+			allyBall1.drawBall(MVP, projection, view, model, programID);
+		}
+
+
+		setLight(programID);
+
+
+
+		//use the shader for the light
+		glUseProgram(programIDLight);
+
+		model = glm::mat4();
+		model = glm::translate(model, lightPos);
+		model = glm::scale(model, glm::vec3(0.0005f)); // a smaller cube
+		MVP = projection * view * model;
+
+		unsigned int transformLoc5 = glGetUniformLocation(programIDLight, "transform");
+		glUniformMatrix4fv(transformLoc5, 1, GL_FALSE, glm::value_ptr(MVP));
+
+		
+		unsigned int transformLoc6 = glGetUniformLocation(programIDLight, "lightColor");
+		glUniform4fv(transformLoc6, 1, glm::value_ptr(lightColor));
+
+		glDrawArrays(GL_QUADS, 0, 24);
+
+	} 
+	
+	// Cleanup VBO
+	glDeleteBuffers(1, &vbo);
+	glDeleteVertexArrays(1, &VertexArrayID);
+	glDeleteProgram(programID);
+
+	// Close OpenGL window and terminate GLFW
+	glfwTerminate();
+
+	return 0;
+}
+
+
+//Process input for camera rotation
+void processKeyboardInput()
+{
+	float cameraSpeed = deltaTime*10;
+
+	//translation
+	if (glfwGetKey(window, GLFW_KEY_W))
+	{
+		camera.keyboardMoveFront(cameraSpeed);
+	}
+	if (glfwGetKey(window, GLFW_KEY_S))
+	{
+		camera.keyboardMoveBack(cameraSpeed);
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_R))
+	{
+		camera.keyboardMoveUp(cameraSpeed);
+	}
+	if (glfwGetKey(window, GLFW_KEY_F))
+	{
+		camera.keyboardMoveDown(cameraSpeed);
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_UP))
+	{
+		camera.rotateOx(cameraSpeed/2);
+	}
+	if (glfwGetKey(window, GLFW_KEY_DOWN))
+	{
+		camera.rotateOx(-cameraSpeed/2);
+	}
+
+}
+
+
+
+
+
